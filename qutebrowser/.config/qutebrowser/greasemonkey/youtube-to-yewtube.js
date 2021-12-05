b89defc96d67137bc00307aa932d8bcbf92a91d6
varaki <varady.akos@gmail.com>
2021-12-05 16:50:40 +0100

    Update qutebrowser config

--- qutebrowser/.config/qutebrowser/greasemonkey/youtube-to-yewtube.js
+++ qutebrowser/.config/qutebrowser/greasemonkey/youtube-to-yewtube.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name           youtube to Yewtube redirector
+// @name           youtube to yewtube redirect
 // @namespace      mamg22's userscripts
 // @match          http://www.youtube.com/*
 // @match          https://www.youtube.com/*
 // @match          http://youtube.com/*
 // @match          https://youtube.com/*
+// @match          http://youtu.be/*
+// @match          https://youtu.be/*
 // @run-at         document-start
 // ==/UserScript==
 location.href=location.href.replace("www.youtube.com","yewtu.be");
