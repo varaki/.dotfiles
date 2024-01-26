5d6052b0ed369c5edea23f95e8dd99f2da33f3da
Ákos Várady <varady.akos@gmail.com>
2024-01-26 11:37:31 +0100

    Add colorizer plugin to neovim

---
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,17 @@
+return {
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+        local colorizer = require("colorizer")
+        colorizer.setup({
+            user_default_options = {
+                rgb_fn = true,
+                buftypes = {
+                    "*",
+                    -- exclude prompt and popup buftypes from highlight
+                    "!prompt",
+                    "!popup",
+                },
+            },
+        })
+    end,
+}
