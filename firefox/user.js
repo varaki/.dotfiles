89b3072e4099c11072a5c5f0532ec028d71cdc8e
Ákos Várady <varady.akos@gmail.com>
2024-02-06 10:12:28 +0100

    Replace DOH provider in firefox config

--- firefox/user.js
+++ firefox/user.js
@@ -56,8 +56,8 @@ user_pref("network.ssl_tokens_cache_capacity", 32768);
 
 /** DNS over HTTPS ***/
 user_pref("network.trr.mode", 2);
-user_pref("network.trr.uri", "https://freedns.controld.com/x-oisd");
-user_pref("network.trr.custom_uri", "https://freedns.controld.com/x-oisd");
+user_pref("network.trr.uri", "https://dnsforge.de/dns-query");
+user_pref("network.trr.custom_uri", "https://dnsforge.de/dns-query");
 user_pref("network.trr.excluded-domains", "ericsson.com,ericsson.se,ericsson.net,opentext.eu,ericy.com,successfactors.eu");
 
 /****************************************************************************
